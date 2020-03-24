04fc540a6b46c23225e2401619671dc382f8ec5d
Anastasia Morozova <morozova.asia@gmail.com>
2020-03-24 16:55:30 -0700

    added test and still working on test routes

--- __tests__/tweet-routes.test.js
+++ __tests__/tweet-routes.test.js
@@ -7,3 +7,21 @@ const mongoose = require('mongoose');
 const Tweet = require('../lib/models/tweet.js');
 
 
+describe('app routes', () => {
+    beforeAll(() => {
+      connect();
+    });
+  
+    beforeEach(() => {
+      return mongoose.connection.dropDatabase();
+    });
+  
+    afterAll(() => {
+      return mongoose.connection.close();
+    });
+
+    it('gets a tweet by id', () => {
+        return Tweet.create({
+            
+        })
+    }
