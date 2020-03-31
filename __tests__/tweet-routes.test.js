72f287c15ae3feadba1ea0bd365fcf267838e2a7
Anastasia Morozova <morozova.asia@gmail.com>
2020-03-30 20:59:55 -0700

    syntax issue

--- __tests__/tweet-routes.test.js
+++ __tests__/tweet-routes.test.js
@@ -6,22 +6,31 @@ const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 const Tweet = require('../lib/models/tweet.js');
 
-
 describe('app routes', () => {
-    beforeAll(() => {
-      connect();
-    });
-  
-    beforeEach(() => {
-      return mongoose.connection.dropDatabase();
-    });
-  
-    afterAll(() => {
-      return mongoose.connection.close();
-    });
+  beforeAll(() => {
+    connect();
+  });
+  beforeEach(()=> {
+    return mongoose.connection.dropDatabase();
+  });
+  afterAll(()=> {
+    return mongoose.connection.close();
+  });
 
-    it('gets a tweet by id', () => {
-        return Tweet.create({
-            
-        })
-    }
+  //test post route 
+  it('creates a comment', async() => {
+    const tweet = await Tweet.create({ handle: 'Black Phillip', text: 'Wouldst thou like to live deliciously?' });
+
+    return request(app)
+      .post('/api/v1/comments/')
+      .send({ tweetId: tweet._id, handle: 'Pinhead', text: 'We have such sights to show you!' });
+  //     .then(res => {
+  //     expect(res.body).toEqual({
+  //       _id: expect.any(String),
+  //       handle: 'Pinhead',
+  //       text: 'We have such sights to show you!',
+  //       __v:0
+  //     })
+  //     })
+  });
+});
